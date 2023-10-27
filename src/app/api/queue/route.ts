1a18de750807cc9273b9f063a2e039df16e9017a
Rot4tion <nhonhoem0123@gmail.com>
2023-10-27 11:29:22 +0700

    trpc setup

--- src/app/api/queue/route.ts
+++ src/app/api/queue/route.ts
@@ -1,7 +1,9 @@
 import brawlAPI from "@/lib/brawlAPI"
+import { trpc } from "@/lib/trpc/client"
+import { serverClient } from "@/lib/trpc/serverClient"
 import util from "@/lib/util"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  return NextResponse.json(await brawlAPI.getLegend())
+  return NextResponse.json(await serverClient.test())
 }
