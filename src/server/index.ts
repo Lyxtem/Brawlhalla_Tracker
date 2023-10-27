58409d0e508a63d946f961831d924913888fa21b
Rot4tion <nhonhoem0123@gmail.com>
2023-10-27 21:17:43 +0700

    test Ky

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,19 +4,19 @@ import { publicProcedure, router } from "./trpc"
 
 export const appRouter = router({
   queue: publicProcedure.query(async () => {
-    // const pathOldRankedData = `public/oldRankedData.json`
-    // const newRankedData: Ranked[] = await brawlAPI.getRankings("1v1", "sea", 1, 10)
-    // let oldRankedData: Ranked[] | undefined
-    // if (!fs.existsSync(pathOldRankedData)) {
-    //   fs.appendFileSync(`public/oldRankedData.json`, JSON.stringify(newRankedData))
-    //   return []
-    // }
-    // oldRankedData = JSON.parse(fs.readFileSync(pathOldRankedData, { encoding: "utf-8" })) as Ranked[]
-    // fs.writeFileSync(pathOldRankedData, JSON.stringify(newRankedData))
-    // return BrawlhallaAPI.trackPlayersInRank(newRankedData, oldRankedData)
+    const pathOldRankedData = `public/oldRankedData.json`
+    const newRankedData: Ranked[] = await brawlAPI.getRankings("1v1", "sea", 1, 20)
+    let oldRankedData: Ranked[] | undefined
+    if (!fs.existsSync(pathOldRankedData)) {
+      fs.appendFileSync(`public/oldRankedData.json`, JSON.stringify(newRankedData))
+      return []
+    }
+    oldRankedData = JSON.parse(fs.readFileSync(pathOldRankedData, { encoding: "utf-8" })) as Ranked[]
+    fs.writeFileSync(pathOldRankedData, JSON.stringify(newRankedData))
+    return BrawlhallaAPI.trackPlayersInRank(newRankedData, oldRankedData)
   }),
   test: publicProcedure.query(async () => {
-    return brawlAPI.getRanking("1v1", "sea", 1)
+    return brawlAPI.getLegend()
   }),
 })
 
