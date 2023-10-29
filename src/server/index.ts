e04f32abce5f04b07d31a6fb5d8a7164836df017
Rot4tion <nhonhoem0123@gmail.com>
2023-10-29 09:57:24 +0700

    add prisma

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,13 @@
 import { z } from "zod"
+import prisma from "../../prisma/client"
 import { publicProcedure, router } from "./trpc"
 
 export const appRouter = router({
   test: publicProcedure.query(async () => {
     return [123]
+  }),
+  prisma: publicProcedure.query(async () => {
+    return await prisma.brawler.create({ data: { brawlhallaId: 1234 } })
   }),
 })
 
