6d9f6820a932343fba7365b2b5b050da4ff42d7c
Rot4tion <nhonhoem0123@gmail.com>
2023-11-03 13:12:52 +0700

    redirect home page to /queue/2v2/sea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation"
 import { GlobalCommands } from "../components/global-commands"
 
 export default async function Page() {
+  redirect("/queue/2v2/sea")
   return (
    <div>Home page</div>
   )
